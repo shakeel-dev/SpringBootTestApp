9220a90478c75f1a2a792fb50f368f3b4b02f7ab
admin123 <admin123@DESKTOP-MN6M8PH>
2020-07-26 00:42:48 -0700

    WH-7896-Logger object is added

--- src/main/java/com/nit/mukram/runners/MyRunner.java
+++ src/main/java/com/nit/mukram/runners/MyRunner.java
@@ -1,14 +1,26 @@
 package com.nit.mukram.runners;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner 
 {
+	private static Logger log=LoggerFactory.getLogger(MyRunner.class);
+	
     @Override
 	public void run(String... args) throws Exception
 	{
     	int a=10;
-		System.out.println("Welcome to Spring Boot App!!"+a);
+    	try {
+    		log.info("STARTED");
+    		System.out.println("Welcome to Spring Boot App!!"+a);
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    	log.info("END");
     }
 }
