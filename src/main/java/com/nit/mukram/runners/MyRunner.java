70506b969fdf80fb089cde21fa1b89c96e3e460e
admin123 <admin123@DESKTOP-MN6M8PH>
2020-07-26 00:33:24 -0700

    WH-8574 variable a is added

--- src/main/java/com/nit/mukram/runners/MyRunner.java
+++ src/main/java/com/nit/mukram/runners/MyRunner.java
@@ -1,17 +1,14 @@
 package com.nit.mukram.runners;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner 
 {
-
-	@Override
+    @Override
 	public void run(String... args) throws Exception
 	{
-		System.out.println("Welcome to Spring Boot App!!");
-		System.out.println("Hello Modified");
+    	int a=10;
+		System.out.println("Welcome to Spring Boot App!!"+a);
     }
-
 }
